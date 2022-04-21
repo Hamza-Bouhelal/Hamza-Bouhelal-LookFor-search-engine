08eaa7a1ed064705073326a73385931e2af23dde
Hamza-Bouhelal <bouhelalhamza@gmail.com>
2022-04-21 16:29:09 +0200

    pushing3

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '73c8483603d00a181b0a1e990d9004d626321de0',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '73c8483603d00a181b0a1e990d9004d626321de0',
+            'dev_requirement' => false,
+        ),
+    ),
+);
